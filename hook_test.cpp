feabd449f50eac4307deab7581000ce4cd878c51
janghanbin <espoirnoa09@naver.com>
2018-10-28 17:51:35 +0900

    modify print values

--- hook_test.cpp
+++ hook_test.cpp
@@ -12,9 +12,8 @@ char* strcpy(char* dest, const char *src)
 {
 	char* rv;
 
-	cout<<"strcpy hooked !!!"<<endl;
-	cout<<"dest : "<<dest<<endl;
-	cout<<"src : " <<src<<endl;
+	cout<<"strcpy hooked by hook_test.so!!!"<<endl;
+	cout<<"Hooked src Contents : " <<src<<endl;
 	
 	origin_strcpy = (char * (*)(char*,const char*))dlsym(RTLD_NEXT, "strcpy");
 	rv = (*origin_strcpy)(dest,src);
